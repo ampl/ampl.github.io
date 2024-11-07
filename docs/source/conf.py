82e80ba6468bbd5d7f0f985b1b7e6a3e92071a3d
Filipe Brandão <fdabrandao@gmail.com>
2024-11-07 14:27:48 +0000

    Theme: rename favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -161,7 +161,7 @@ html_logo = "https://raw.githubusercontent.com/ampl/ampl.github.io/master/themes
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 # html_favicon = None
-html_favicon = "https://raw.githubusercontent.com/ampl/ampl.github.io/master/themes/static/ampl-favicon-circle.png"
+html_favicon = "https://raw.githubusercontent.com/ampl/ampl.github.io/master/themes/static/ampl-favicon.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
